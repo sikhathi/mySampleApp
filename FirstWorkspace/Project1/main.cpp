0007ff445ac0e27a38a6fa4920c2c2be0d73bfed
sikhathi <86sikhathi@gmail.com>
2020-07-15 20:36:38 +0200

    initial commit

---
+++ FirstWorkspace/Project1/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main (){
+    std::cout<<"hello testing"<<std::endl;
+    return 0;
+}
---
+++ MyWorkspace/Project2/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(){
+    std::cout<<"my template"<<std::endl;
+    return 0;
+}
